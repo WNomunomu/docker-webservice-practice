0c5b681cc0d55775282385af62b9f6f6d1e0b7be
WNomunomu <kohsei.nmr@gmail.com>
2024-11-25 16:20:55 +0900

    Write sql statements for db container

---
+++ work/docker/db/init/init-user.sql
@@ -0,0 +1,4 @@
+create table user ( id int, name varchar(32), email varchar(32) );
+insert user ( id, name, email ) values ( 1, 'WNomunomu', 'wnomunomu@example.com' );
+insert user ( id, name, email ) values ( 2, 'Jane Doe', 'jane@example.com' );
+insert user ( id, name, email ) values ( 3, 'John Doe', 'john@example.com' );
